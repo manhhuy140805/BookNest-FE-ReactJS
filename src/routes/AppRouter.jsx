9d92f995fe267013fed9053147c8750faba88da8
DESKTOP-NH61KMC\ADMIN <ccaotthihhan@gmail.com>
2026-03-25 11:47:15 +0700

    Khung render danh sách sách

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import Home from "../pages/Home/Home";
+import BookList from "../pages/Books/BookList";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
         <Route path="/login" element={<Login />} />
         <Route path="/auth/callback" element={<Login />} />
         <Route path="/register" element={<Register />} />
